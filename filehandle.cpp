2d5ccc1509b683ba5f0a4f43aa9e68283254e890
hydra <nguyentrungduc08@gmail.com>
2018-02-27 14:09:14 +0700

    add file handle

--- filehandle.cpp
+++ filehandle.cpp
@@ -4,3 +4,30 @@ FileHandle::FileHandle()
 {
 
 }
+
+FileHandle::~FileHandle(){
+
+}
+
+int FileHandle::openFile(std::string fileName){
+    this->readStream.open(fileName.c_str(), std::ios::in|std::ios::binary); // modes for binary file  |std::ios::ate
+    if (this->readStream.fail()) {
+        std::cout << "Reading file '" << fileName << "' failed!" << std::endl; //  strerror(errno) <<
+        return (false);
+    }
+    if (this->readStream.is_open()) {
+        return (true);
+    }
+    std::cerr << "Unable to open file '" << fileName << " '" << std::endl; // << strerror(errno)
+    return (false);
+}
+
+char* FileHandle::readFileBlock(unsigned long &sizeInBytes){
+    this->readStream.seekg(0, std::ios::end);
+    std::ifstream::pos_type size = this->readStream.tellg();
+    sizeInBytes = (unsigned long) size;
+
+    this->readStream.seekg(0, std::ios::beg);
+
+    char * memblock = new char[size];
+}
--- filehandle.h
+++ filehandle.h
@@ -1,11 +1,19 @@
 #ifndef FILEHANDLE_H
 #define FILEHANDLE_H
 
+#include "filesys.h"
 
 class FileHandle
 {
 public:
     FileHandle();
+    ~FileHandle();
+    bool openFile(std::string fileName);
+    char* readFileBlock(unsigned long &sizeInBytes);
+
+private:
+    std::ofstream writeStream; //stream to read data from socket and write to file
+    std::ifstream readStream; //stream to read from file and send to socket
 };
 
 #endif // FILEHANDLE_H
